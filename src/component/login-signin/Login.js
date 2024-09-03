a5461c747c6418f3e32a7187cc910df2ee1dd1c6
Mohan Singh Mahecha <67053538+mohansinghmahecha@users.noreply.github.com>
2024-09-03 16:13:55 +0530

    Add files via upload

--- src/component/login-signin/Login.js
+++ src/component/login-signin/Login.js
@@ -14,13 +14,16 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://entertainment-backend-1-5p0s.onrender.com/api/users/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+      const response = await fetch(
+        "https://entertainment-backend-1-5p0s.onrender.com/api/users/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
 
       const data = await response.json();
 
